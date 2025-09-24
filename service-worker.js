8e901a0ca1d18533def88b2af7947d3b15a420a0
ArunGovil <arun.govil@unifize.com>
2025-09-24 20:10:00 +0530

    Add PWA config

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = "v1";
+const cacheAssets = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./pwa-icon-192.png",
+  "./pwa-icon-512.png",
+];
+
+// Install Event
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(cacheAssets)),
+  );
+});
+
+// Fetch Event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
